66f6bccfdaf64105ecab31b02aeb0db5a0baf56e
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-14 21:40:45 -0300

    CarList Class Updated & CarCreate Added

--- CarCatalog/urls.py
+++ CarCatalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from CarCatalog.views import CarList
+from CarCatalog.views import CarList, CarCreate
 
 urlpatterns = [
   path('', CarList.as_view(), name='car_list'),
+  path('car/new/', CarCreate.as_view(), name='car_new'),
 ]
--- CarCatalog/views.py
+++ CarCatalog/views.py
@@ -1,20 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from django.views import View
 
+
 from .models import Car
+from .forms import CarForm
 
 
-# Create your views here.
-
-class CarList(View):
+# Django Class-Based Views
+class CarList(View): 
   
 
-  def get(self, request, *args, **kwargs):
-    template_name = 'CarCatalog/car_list.html'
+  def get(self, request, *args, **kwargs): 
     car_list = Car.objects.all()
-    
-    return render(request, template_name, {'car_list': car_list})
+    template_name = 'CarCatalog/car_list.html' 
+    return render(request, template_name, {'car_list': car_list} ) 
 
+
+class CarCreate(View): 
   
+  form_class = CarForm 
+
+  def get(self, request, *args, **kwargs): 
+    form = self.form_class 
+    template_name = 'CarCatalog/car_edit.html' 
+    return render(request, template_name, {'form': form })
+
+  def post(self, request, *args, **kwargs): 
+    
+    template_name = 'CarCatalog/car_edit.html' 
+    form = self.form_class(request.POST) 
+    if form.is_valid():
+      car = form.save(commit=False)
+      car.save()
+      return redirect('car_list')
+    return render(request, template_name, {'form': form })
 
 
+# Django Generic Class-Based Views
